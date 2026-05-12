641d55e64cac019f20d8c130f011630573af1da7
mjissoy <mjissoy@gmail.com>
2026-05-13 05:05:21 +1000

    empty class

---
+++ src/main/java/net/thejadeproject/ascension/refactor_packages/events/physiques/EssenceBonePhysiqueEvolutionEvents.java
@@ -0,0 +1,12 @@
+package net.thejadeproject.ascension.refactor_packages.events.physiques;
+
+public class EssenceBonePhysiqueEvolutionEvents {
+
+    /*
+     TODO:
+      Evolution from Mortal to Special
+      to Heavenly to Divine.
+      IDK haven't decided yet
+     */
+
+}
