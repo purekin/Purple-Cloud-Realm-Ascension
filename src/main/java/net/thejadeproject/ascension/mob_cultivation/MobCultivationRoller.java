f2494c66f553d21147d46921d35ce818877e9223
mjissoy <mjissoy@gmail.com>
2026-05-13 05:00:30 +1000

    did absolutely nothing here ...................................................................................................... added golden core 3 to the default mob spawn pool

--- src/main/java/net/thejadeproject/ascension/mob_cultivation/MobCultivationRoller.java
+++ src/main/java/net/thejadeproject/ascension/mob_cultivation/MobCultivationRoller.java
@@ -10,7 +10,7 @@ public final class MobCultivationRoller {
     private static final Random RANDOM = new Random();
 
     private static final List<WeightedMobCultivation> DEFAULT_WEIGHTS = List.of(
-            new WeightedMobCultivation(MobCultivationList.get("mortal", 1), 30),
+            new WeightedMobCultivation(MobCultivationList.get("mortal", 1), 29),
             new WeightedMobCultivation(MobCultivationList.get("mortal", 2), 18),
             new WeightedMobCultivation(MobCultivationList.get("mortal", 3), 12),
 
@@ -23,7 +23,10 @@ public final class MobCultivationRoller {
             new WeightedMobCultivation(MobCultivationList.get("formation_establishment", 3), 2),
 
             new WeightedMobCultivation(MobCultivationList.get("golden_core", 1), 1),
-            new WeightedMobCultivation(MobCultivationList.get("golden_core", 2), 1)
+            new WeightedMobCultivation(MobCultivationList.get("golden_core", 2), 1),
+            new WeightedMobCultivation(MobCultivationList.get("golden_core", 3), 1)
+
+
     );
 
     private MobCultivationRoller() {
